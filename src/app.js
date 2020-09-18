cf2e2c9237313262911daed2e9e4a616e01e0384
Michael Oldacre <mloldacre@live.com>
2020-09-18 13:19:37 -0400

    passing interview test

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./middleware/error-handler')
-const TodoService = require('./todo/todo-service')
-const xss = require('xss')
-const jsonParser = express.json()
-const path = require('path')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./middleware/error-handler');
+const TodoService = require('./todo/todo-service');
+const xss = require('xss');
+const jsonParser = express.json();
+const path = require('path');
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -18,30 +18,59 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption, {
   skip: () => NODE_ENV === 'test',
-}))
-app.use(cors())
-app.use(helmet())
+}));
+app.use(cors());
+app.use(helmet());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 const serializeTodo = todo => ({
   id: todo.id,
   title: xss(todo.title),
   completed: todo.completed
-})
+});
 
 app
   .route('/v1/todos')
-  .get(/* Your code here */)
-  .post(/* Your code here */)
+  .get((req, res, next) => {
+    TodoService.getTodos(
+      req.app.get('db')
+    )
+      .then(todos => {
+        res.json(todos.map(serializeTodo));
+      });
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { title } = req.body;
+    const newTodo = { title };
+
+    for (const [key, value] of Object.entries(newTodo)) {
+      if(value == null) {
+        return res.status(400).json({
+          error: {message: 'Bad Request'}
+        });
+      }
+    }
+
+    TodoService.insertTodo(
+      req.app.get('db'),
+      newTodo
+    )
+      .then(todo => {
+        res.status(201)
+          .location(path.posix.join(req.originalUrl, `/${todo.id}`))
+          .json(serializeTodo(todo));
+      })
+      .catch(next);
+  });
 
 app
   .route('/v1/todos/:todo_id')
   .all((req, res, next) => {
-    if(isNaN(parseInt(req.params.todo_id))) {
+    if (isNaN(parseInt(req.params.todo_id))) {
       return res.status(404).json({
         error: { message: `Invalid id` }
-      })
+      });
     }
     TodoService.getTodoById(
       req.app.get('db'),
@@ -51,15 +80,15 @@ app
         if (!todo) {
           return res.status(404).json({
             error: { message: `Todo doesn't exist` }
-          })
+          });
         }
-        res.todo = todo
-        next()
+        res.todo = todo;
+        next();
       })
-      .catch(next)
+      .catch(next);
   })
   .get((req, res, next) => {
-    res.json(serializeTodo(res.todo))
+    res.json(serializeTodo(res.todo));
   })
   .delete((req, res, next) => {
     TodoService.deleteTodo(
@@ -67,21 +96,21 @@ app
       req.params.todo_id
     )
       .then(numRowsAffected => {
-        res.status(204).end()
+        res.status(204).end();
       })
-      .catch(next)
+      .catch(next);
   })
   .patch(jsonParser, (req, res, next) => {
-    const { title, completed } = req.body
-    const todoToUpdate = { title, completed }
+    const { title, completed } = req.body;
+    const todoToUpdate = { title, completed };
 
-    const numberOfValues = Object.values(todoToUpdate).filter(Boolean).length
+    const numberOfValues = Object.values(todoToUpdate).filter(Boolean).length;
     if (numberOfValues === 0)
       return res.status(400).json({
         error: {
           message: `Request body must content either 'title' or 'completed'`
         }
-      })
+      });
 
     TodoService.updateTodo(
       req.app.get('db'),
@@ -89,12 +118,12 @@ app
       todoToUpdate
     )
       .then(updatedTodo => {
-        res.status(200).json(serializeTodo(updatedTodo[0]))
+        res.status(200).json(serializeTodo(updatedTodo[0]));
       })
-      .catch(next)
-  })
+      .catch(next);
+  });
 
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
